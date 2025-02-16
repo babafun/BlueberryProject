9af6a847a67207309ba2f6c801444efa61cd1c15
babafun <bralen.moras2580@outlook.com>
2025-02-16 15:59:27 +0000

    LEVELS UPDATE!!!
    New level made.

--- script.js
+++ script.js
@@ -5,13 +5,38 @@ const createGrassTexture = () => {
     img.src = 'grass.png';
     return img;
 };
-
-const platforms = [
-    { texture: createGrassTexture(), x: 0, y: 550, width: 800, height: 200 }, // Ground platform
+const levels = [
+    [{ texture: createGrassTexture(), x: 0, y: 550, width: 800, height: 200 }, // Ground platform
     { texture: createGrassTexture(), x: 400, y: 450, width: 200, height: 20 },
     { texture: createGrassTexture(), x: 100, y: 300, width: 250, height: 20 },
-    { texture: createGrassTexture(), x: 300, y: 100, width: 300, height: 20 }
-];
+    { texture: createGrassTexture(), x: 300, y: 100, width: 300, height: 20 }],
+
+
+    [{ texture: createGrassTexture(), x: 0, y: 550, width: 800, height: 200 }, // Ground platform
+
+    // New tighter platforms
+    { texture: createGrassTexture(), x: 100, y: 450, width: 150, height: 20 },
+    { texture: createGrassTexture(), x: 350, y: 350, width: 150, height: 20 },
+    { texture: createGrassTexture(), x: 600, y: 250, width: 200, height: 20 },
+
+    // Higher platforms with more jumps required
+    { texture: createGrassTexture(), x: 200, y: 150, width: 250, height: 20 },
+    { texture: createGrassTexture(), x: 500, y: 100, width: 150, height: 20 },
+
+    // Smaller platforms at higher positions to test accuracy
+    { texture: createGrassTexture(), x: 50, y: 75, width: 100, height: 20 },
+    { texture: createGrassTexture(), x: 650, y: 50, width: 100, height: 20 },
+
+    // Platforms with gaps in between that require jumping across
+    { texture: createGrassTexture(), x: 400, y: 450, width: 150, height: 20 },
+    { texture: createGrassTexture(), x: 580, y: 400, width: 150, height: 20 },
+    { texture: createGrassTexture(), x: 50, y: 250, width: 150, height: 20 },
+
+    // More challenging vertical platforming
+    { texture: createGrassTexture(), x: 250, y: 150, width: 100, height: 20 },
+    { texture: createGrassTexture(), x: 300, y: 50, width: 100, height: 20 },
+]];
+const platforms = levels[1]; // Change the level index to test different levels
 
 // Ensure all platform textures are loaded before starting the game
 let texturesLoaded = 0;
