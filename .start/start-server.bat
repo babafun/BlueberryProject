0d00334c35ac5d140fabad0ce4fe17a079b2990c
Bralen Moras <bralen.moras2580@outlook.com>
2025-05-05 12:55:32 +0100

    Update

--- .start/start-server.bat
+++ .start/start-server.bat
@@ -6,7 +6,7 @@ set "scriptDir=%~dp0"
 
 :: Run the PowerShell script from the same folder
 echo Attempting to start the server. Do not close this window, as doing so will stop the server.
-echo If you want to stop the server, it is much safer to use the keyboard shortcut Ctrl+C before closing this window.
+echo If you want to stop the server, it is much safer to use the keyboard shortcut Ctrl+C to close this window.
 powershell -ExecutionPolicy Bypass -File "%scriptDir%local-server-manager-windows.ps1"
 if %errorlevel% neq 0 (
     echo Failed to start the server.
